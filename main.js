2cacf09e1cf2a82a38c970eb50aa3605b91a2f7d
DreamSky26 <83844480+DreamSky26@users.noreply.github.com>
2022-02-10 21:06:58 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassification('https://teachablemachine.withgoogle.com/models/XWMOlgSyZ/model.json', modelReady);
+}
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
+function gotResults(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+    }
+}
